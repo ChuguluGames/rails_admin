adc8ba4faadea767969a86cc7b4fd9758501d507
AdrienGiboire <adrien.giboire@yahoo.com>
2012-03-26 14:55:51 +0200

    ADD translation for unauthorized account

--- lib/generators/rails_admin/templates/controllers/omniauth_callbacks_controller.rb
+++ lib/generators/rails_admin/templates/controllers/omniauth_callbacks_controller.rb
@@ -2,12 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
+    if @user and @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:error] = I18n.t "devise.omniauth_callback.failure", :kind => "Google", :reason => I18n.t "devise.failure.inactive"
+      redirect_to new_user_session_url
     end
   end
 end
